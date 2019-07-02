b13fc659dab193213578f017b4d5f2a91d63cd72
Francisco Rubin <pancho_111203@hotmail.com>
2019-07-02 17:51:58 +0800

    fixed gpu issue

--- setup.py
+++ setup.py
@@ -27,12 +27,7 @@ setup(
         [
          'sparseconvnet/SCN/cuda.cu', 'sparseconvnet/SCN/sparseconvnet_cuda.cpp', 'sparseconvnet/SCN/pybind.cpp'],
         include_dirs=[conda_include_dir, this_dir+'/sparseconvnet/SCN/'],
-        extra_compile_args=extra)
-      if torch.cuda.is_available()  else
-      CppExtension('sparseconvnet.SCN',
-        ['sparseconvnet/SCN/pybind.cpp', 'sparseconvnet/SCN/sparseconvnet_cpu.cpp'],
-        include_dirs=[conda_include_dir, this_dir+'/sparseconvnet/SCN/'],
-        extra_compile_args=extra['cxx'])],
+        extra_compile_args=extra)],
     cmdclass={'build_ext': BuildExtension},
     zip_safe=False,
 )
